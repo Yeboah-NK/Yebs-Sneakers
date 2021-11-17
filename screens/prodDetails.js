50fd3b94328f79201143d7dbacfa8924aa7f7344
yeboah-NK <yebshimself01@gmail.com>
2021-11-17 21:11:58 +0800

    update

--- screens/prodDetails.js
+++ screens/prodDetails.js
@@ -19,7 +19,7 @@ export default function ProdDetails({navigation, route}) {
                 <Text style={styles.about}>{item.about}</Text>
                 <Text>SIZE</Text>
                 <FlatList />
-                <TouchableOpacity>
+                <TouchableOpacity  onPress={()=>{navigation.navigate('Cart', item)}}>
                     <View style={{marginTop: '10%', backgroundColor:'dodgerblue',  flexDirection:'row', justifyContent:'space-around', alignSelf:'center', borderWidth: 1,borderRadius: 25,width: 200,alignItems: 'center',padding: 5,}}>
                     <Text style={{color:'white',}}>+</Text>
                     <Text style={{color:'white',}}>ADD TO CART</Text>
